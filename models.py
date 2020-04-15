da1c356252465e8daaa283e70760307b5116b936
MattiooFR <13595614+MattiooFR@users.noreply.github.com>
2020-04-15 18:24:34 +0200

    added requirements.txt

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ import json
 import os
 
 database_path = os.environ['postgres://shrjmzoqzfoggk:be275871d8522287f35040ec70230c42aefbf7c2c6511b951a3a290875c64028@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d9mbs1upgrn899']
-local_db_path = "postgres://mattioo@localhost:5432/1pic1day"
 
 db = SQLAlchemy()
 
